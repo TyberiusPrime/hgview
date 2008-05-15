3a68e24fda89064672f98c869a3fdfb9f1d2b038
David Douard <david.douard@logilab.fr>
2008-05-15 10:00:55 +0200

    prepare de 0.3.1 version

--- hgview/__pkginfo__.py
+++ hgview/__pkginfo__.py
@@ -1,7 +1,5 @@
-# pylint: disable-msg=W0622,C0103
-# Copyright (c) 2003-2007 LOGILAB S.A. (Paris, FRANCE).
-# http://www.logilab.fr/ -- mailto:contact@logilab.fr
-#
+# pylint: disable-msg=W0622
+# coding: iso-8859-1
 # This program is free software; you can redistribute it and/or modify it under
 # the terms of the GNU General Public License as published by the Free Software
 # Foundation; either version 2 of the License, or (at your option) any later
@@ -14,49 +12,49 @@
 # You should have received a copy of the GNU General Public License along with
 # this program; if not, write to the Free Software Foundation, Inc.,
 # 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
-"""hgview packaging information"""
-
-modname = 'hgview'
-
-numversion = (0, 3, 0)
-version = '.'.join([str(num) for num in numversion])
-
-license = 'GPL'
-copyright = '''Copyright (c) 2003-2007 Ludovic Aubry (ludovic.aubry@logilab.fr).
-Copyright (c) 2003-2007 LOGILAB S.A. (Paris, FRANCE).
-http://www.logilab.fr/ -- mailto:contact@logilab.fr'''
-
-short_desc = "a mercurial interactive history viewer"
-long_desc = """\
- Hgview is a mercurial interactive history viewer, written in
- Python/GTK and PyQt. Its purpose is similar to the hgk tool of
- mercurial. It has been written with efficiency in mind when dealing
- with big repositories (it can happily be used to browse Linux kernel
- source code repository).
+"""Copyright (c) 2000-2008 LOGILAB S.A. (Paris, FRANCE).
+http://www.logilab.fr/ -- mailto:contact@logilab.fr  
 """
 
-author = "Ludovic Aubry"
-author_email = "ludovic.aubry@logilab.fr"
+import glob
+modname = 'hgview'
+numversion = (0, 3, 1)
+version = '.'.join([str(num) for num in numversion])
 
-web = "http://www.logilab.org/project/name/%s" % modname
+
+license = 'GPL'
+copyright = '''Copyright © 2007-2008 LOGILAB S.A. (Paris, FRANCE).
+http://www.logilab.fr/ -- mailto:contact@logilab.fr'''
+
+short_desc = "mercurial interactive history viewer"
+
+long_desc = """
+ Its purpose is similar to the hgk tool of mercurial, and it has been
+ written with efficiency in mind when dealing with big repositories
+ (it can happily be used to browse Linux kernel source code
+ repository).
+"""
+
+author = "Logilab"
+author_email = "devel@logilab.fr"
+
+# TODO - publish
+web = "http://www.logilab.org/projects/%s" % modname
 ftp = "ftp://ftp.logilab.org/pub/%s" % modname
-mailinglist = "mailto://python-projects@logilab.org"
 
+
+scripts = ['bin/hgview']
+debian_name = 'hgview'
+debian_maintainer = 'Alexandre Fayolle' 
+debian_maintainer_email = 'alexandre.fayolle@logilab.fr'
+pyversions = ["2.4", "2.5"]
+
+debian_handler = 'python-dep-standalone'
+ 
 from os.path import join
-scripts = [join('bin', filename)
-           for filename in ('hgview',)]
-
+include_dirs = [] #join('tests', 'data'), join('tests', 'packages')]
 data_files = [('share/hgview', ['hgview/gtk/hgview.glade',
                                  'hgview/qt4/hgview.ui']),
               ('share/man/man1', ['hgview.1',
                                  ]),]
-
-## include_dirs = [join('test', 'input'),
-##                 join('test', 'messages'),
-##                 join('test', 'rpythonmessages'),
-##                 join('test', 'regrtest_data')]
-
-pyversions = ["2.3", "2.4", "2.5"]
-
-debian_uploader = 'Alexandre Fayolle <afayolle@debian.org>'
 
