8cf2f925884f8def5f6f169ea72cf6f30562bf93
Graziella Toutoungis <graziella.toutoungis@logilab.fr>
2008-11-14 17:02:27 +0100

    prepare 0.9.1 version

--- hgview/__pkginfo__.py
+++ hgview/__pkginfo__.py
@@ -18,7 +18,7 @@ http://www.logilab.fr/ -- mailto:contact@logilab.fr
 
 import glob
 modname = 'hgview'
-numversion = (0, 9, 0)
+numversion = (0, 9, 1)
 version = '.'.join([str(num) for num in numversion])
 
 
