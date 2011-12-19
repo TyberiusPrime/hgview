fecce4d10272238acf3c5abeeac2b8747bf25d82
Pierre-Yves David <pierre-yves.david@logilab.fr>
2011-12-19 16:11:24 +0100

    [hidden] fix infinite loop when hidden changeset.
    Introduced by 4eae0844205b40a0252eefa89cb28483b77b3b06

--- hgviewlib/hggraph.py
+++ hgviewlib/hggraph.py
@@ -162,6 +162,10 @@ def revision_grapher(repo, start_rev=None, stop_rev=0, branch=None, follow=False
     while curr_rev is None or curr_rev >= stop_rev:
         # Compute revs and next_revs.
         if (not show_hidden) and curr_rev in hiddenrevs:
+            if curr_rev is None:
+                curr_rev = len(repo.changelog)
+            else:
+                curr_rev -= 1
             continue
         if curr_rev not in revs:
             if branch:
