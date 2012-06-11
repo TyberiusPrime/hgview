f043b9ed7a73ea98e58778581770f8213458a006
Takumi IINO <trot.thunder@gmail.com>
2012-06-11 12:35:09 +0200

    [tui] ctx.description(): always tries to convert to unicode (closes #98647)
    tries to convert to unicode like hgviewlib.hggraph.getlog.

--- hgviewlib/curses/hgrepoviewer.py
+++ hgviewlib/curses/hgrepoviewer.py
@@ -29,7 +29,7 @@ from urwid import AttrWrap, Pile, Columns, SolidFill, signals
 from urwid.util import is_mouse_press
 
 from hgviewlib.hggraph import HgRepoListWalker
-from hgviewlib.util import exec_flag_changed, isbfile
+from hgviewlib.util import exec_flag_changed, isbfile, tounicode
 
 from hgviewlib.curses.graphlog import RevisionsWalker
 from hgviewlib.curses.manifest import ManifestWalker
@@ -204,7 +204,7 @@ class ContextViewer(Columns):
         flag = ''
         if filename is None: # source content is the changeset description
             wrap = 'space' # Do not cut description and wrap content
-            data = ctx.description()
+            data = tounicode(ctx.description())
             if pygments:
                 lexer = lexers.RstLexer()
         else: # source content is a file
