a1c350901859867d2e32ba5942cdcd8537fba9ac
Pierre-Yves David <pierre-yves.david@logilab.fr>
2012-08-06 17:11:23 +0200

    [pkg] properly fix the previous changeset published prematurely

--- hgviewlib/__pkginfo__.py
+++ hgviewlib/__pkginfo__.py
@@ -1,4 +1,4 @@
-# pylint: disable-msg=W0622
+# pylint: disable=W0622
 # coding: iso-8859-1
 # This program is free software; you can redistribute it and/or modify it under
 # the terms of the GNU General Public License as published by the Free Software
