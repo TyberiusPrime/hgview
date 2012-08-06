e1b6b0ccbec544565922cfe548c32e16ebc8f8be
Pierre-Yves David <pierre-yves.david@logilab.fr>
2012-08-06 16:40:47 +0200

    [pkg] update copyright notice to 2012

--- hgviewlib/__pkginfo__.py
+++ hgviewlib/__pkginfo__.py
@@ -11,7 +11,7 @@
 #
 # You should have received a copy of the GNU General Public License along with
 # this program.  If not, see <http://www.gnu.org/licenses/>.
-"""Copyright (c) 2000-2011 LOGILAB S.A. (Paris, FRANCE).
+"""Copyright (c) 2000-2012 LOGILAB S.A. (Paris, FRANCE).
 http://www.logilab.fr/ -- mailto:contact@logilab.fr
 """
 
