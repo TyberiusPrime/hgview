766d4266c2ac53e5e6f2a5c3ee3f6d8b00274631
Pierre-Yves David <pierre-yves.david@logilab.fr>
2012-12-20 15:40:18 +0100

    [qt] fix error message fo failed resource compilation

--- hgviewlib/qt4/__init__.py
+++ hgviewlib/qt4/__init__.py
@@ -33,7 +33,7 @@ pyfile = osp.join(curdir, "hgqv_rc.py")
 rcfile = osp.join(curdir, "hgqv.qrc")
 if should_rebuild(rcfile, pyfile):
     if os.system('pyrcc4 %s -o %s' % (rcfile, pyfile)):
-        print "ERROR: Cannot convert the resource file '%s' into a python module."
+        print "ERROR: Cannot convert the resource file '%s' into a python module." % rcfile
         print "Please check the PyQt 'pyrcc4' tool is installed, or do it by hand running:"
         print "pyrcc4 %s -o %s" % (rcfile, pyfile)
 
