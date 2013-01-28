bb354a0eaf281de8e6a714969e587d7564cb5c09
Pierre-Yves David <pierre-yves.david@logilab.fr>
2013-01-28 16:49:43 +0100

    [pkg] prepare version 1.7.1

--- hgviewlib/__pkginfo__.py
+++ hgviewlib/__pkginfo__.py
@@ -17,7 +17,7 @@ http://www.logilab.fr/ -- mailto:contact@logilab.fr
 
 import glob
 distname = modname = 'hgview'
-numversion = (1, 7, 0)
+numversion = (1, 7, 1)
 version = '.'.join([str(num) for num in numversion])
 
 
